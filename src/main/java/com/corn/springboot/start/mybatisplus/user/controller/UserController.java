999ff3c0a6fdf1bafe6fafddd429a93657dc26ec
suyiming <suyiming3333@gmail.com>
2021-04-23 00:16:16 +0800

    redis test

--- src/main/java/com/corn/springboot/start/mybatisplus/user/controller/UserController.java
+++ src/main/java/com/corn/springboot/start/mybatisplus/user/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.corn.springboot.start.mybatisplus.user.controller;
 
 
+import com.corn.springboot.start.mybatisplus.user.entity.User;
+import com.corn.springboot.start.mybatisplus.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * <p>
@@ -17,5 +24,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @RequestMapping("/list")
+    public List<User> list(){
+        List<User> users = userService.list();
+        redisTemplate.opsForValue().set("list",users);
+        return users;
+    }
+
+    @RequestMapping("/listFromRedis")
+    public List<User> listFromRedis(){
+        List<User> list = (List<User>) redisTemplate.opsForValue().get("list");
+        return list;
+    }
+
+    @RequestMapping("/initDataToRedis")
+    public void initDataToRedis(){
+        List<User> users = userService.list();
+        users.stream().forEach(c->{
+            redisTemplate.opsForZSet().add("zlist",c.getName(),c.getAge());
+        });
+    }
+
+    @RequestMapping("/rank")
+    public String rank(){
+        Set<String> set = redisTemplate.opsForZSet().rangeWithScores("zlist", 1L, 20L);
+        return null;
+    }
 }
 
